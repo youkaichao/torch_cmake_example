742f5ea60d85055c7095268e2cbdf6acd59966b5
youkaichao <youkaichao@gmail.com>
2025-06-13 10:13:12 +0800

    add
    Signed-off-by: youkaichao <youkaichao@gmail.com>

---
+++ src.cpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <torch/all.h>
+
+torch::Tensor sin_and_add(torch::Tensor x, torch::Tensor y) {
+    return torch::sin(x) + y;
+}
+
+TORCH_LIBRARY(torch_extension, m) {
+    m.def("sin_and_add", &sin_and_add, "Compute sin(x) + y");
+} 
